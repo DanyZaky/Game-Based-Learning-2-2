6ba6ff3e2167361a31b5bda9edbbcbf41a3fa182
Dany Zaky <danyzakydany123@gmail.com>
2022-03-17 11:34:28 +0700

    fixing winlose

--- game-based-laerning/Assets/Scripts/Fishing.cs
+++ game-based-laerning/Assets/Scripts/Fishing.cs
@@ -6,9 +6,9 @@ using TMPro;
 
 public class Fishing : MonoBehaviour
 {
-    [SerializeField] private GameObject isFishingText, startFishButton, catchButton, attackButton, IkanPaus;
+    [SerializeField] private GameObject isFishingText, startFishButton, catchButton, EndFishedButton, stopFishingButton, attackButton, IkanPaus;
 
-    [SerializeField] private TextMeshProUGUI attackTimeRemaining;
+    [SerializeField] private TextMeshProUGUI attackTimeRemaining, winLoseText;
     private float timeCountdown;
     private float timeCountdownCounter;
     private bool isCountdown;
@@ -23,6 +23,10 @@ public class Fishing : MonoBehaviour
         isFishingText.SetActive(false);
         catchButton.SetActive(false);
         attackButton.SetActive(false);
+        IkanPaus.SetActive(false);
+
+        attackTimeRemaining.gameObject.SetActive(false);
+        winLoseText.gameObject.SetActive(false);
 
         isCountdown = false;
         
@@ -31,6 +35,8 @@ public class Fishing : MonoBehaviour
 
         HPFish = 10f;
         HPFishCounter = HPFish;
+
+        stopFishingButton.SetActive(false);
     }
 
     void Update()
@@ -58,17 +64,34 @@ public class Fishing : MonoBehaviour
         {
             isCountdown = false;
             Debug.Log("Lose");
+            isFished("Lose", 1f);
+            EndFishedButton.SetActive(true);
+            stopFishingButton.SetActive(true);
+            HPFishCounter = HPFish;
         }
 
         if (HPFishCounter <= 0)
         {
             isCountdown = false;
             Debug.Log("Win");
+            isFished("Win", 1f);
+            EndFishedButton.SetActive(true);
+            stopFishingButton.SetActive(true);
+            HPFishCounter = HPFish;
         }
+    }
+
+    private void isFished(string winLose, float endDuration)
+    {
+        winLoseText.gameObject.SetActive(true);
+        winLoseText.SetText(winLose);
+        IkanPaus.SetActive(false);
+        attackButton.SetActive(false);
     }
 
     private IEnumerator isFishing(float FishingDuration)
     {
+        EndFishedButton.SetActive(false);
         startFishButton.SetActive(false);
         isFishingText.SetActive(true);
         yield return new WaitForSecondsRealtime(FishingDuration);
@@ -79,6 +102,7 @@ public class Fishing : MonoBehaviour
 
     public void StartFishingButton()
     {
+        
         StartCoroutine(isFishing(2f));
     }
 
@@ -86,13 +110,34 @@ public class Fishing : MonoBehaviour
     {
         catchButton.SetActive(false);
         attackButton.SetActive(true);
+        
+        isCountdown = true;
+        attackTimeRemaining.gameObject.SetActive(true);
+
+        IkanPaus.SetActive(true);
     }
 
     public void AttackButton()
     {
-        isCountdown = true;
+        
         HPFishCounter -= 1f;
         HPFishFill.fillAmount = HPFishCounter / 10;
         Debug.Log("HP"+ HPFishCounter);
+    }
+
+    public void FishedButton()
+    {
+        EndFishedButton.SetActive(false);
+        winLoseText.gameObject.SetActive(false);
+        stopFishingButton.SetActive(false);
+        StartCoroutine(isFishing(2f));
+    }
+
+    public void EndFishingButton()
+    {
+        EndFishedButton.SetActive(false);
+        winLoseText.gameObject.SetActive(false);
+        stopFishingButton.SetActive(false);
+        startFishButton.SetActive(true);
     }
 }
